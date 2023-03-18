f25083ff761de302fdc5eae207343c1064a4dd75
Sam Morton <sammorton44@gmail.com>
2023-03-18 07:54:44 -0700

    basic components dir for the components library and bare minimum react app to test components

---
+++ src/components/index.ts
@@ -0,0 +1,5 @@
+import OurlitButton from './src/OurlitButton';
+
+export {
+    OurlitButton
+};
---
+++ src/components/src/OurlitButton.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+// import './ourlit-button.css';
+
+// import { OurlitButtonProps } from '@types';
+
+const OurlitButton = ({ text }: { text: string }) => (
+    <button className="ourlit-button">{text}</button>
+);
+
+export default OurlitButton;
