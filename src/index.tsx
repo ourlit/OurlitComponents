1fefb37a6ffd229f57450adc413f0a56d5eea6c9
Sam Morton <sammorton44@gmail.com>
2023-03-19 13:46:24 -0700

    simple test code for the button

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,32 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 const App = () => (
-    <OurlitButton text="Sam" />
+    <>
+      <OurlitButton
+        text="Sam"
+        color="primaryBlue"
+        size="medium"
+        variant="contained"
+      />
+      <OurlitButton
+        text="Sam"
+        color="primaryBlue"
+        size="large"
+        variant="contained"
+      />
+      <OurlitButton
+        text="Sam"
+        color="primaryRed"
+        size="small"
+        variant="outlined"
+      />
+      <OurlitButton
+        text="Sam"
+        color="primaryRed"
+        size="large"
+        variant="text"
+      />
+    </>
 );
 
 ReactDOM.render(
